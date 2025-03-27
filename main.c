dbceff27ec313ca835154c2019742e1158a32735
Luis Felipe Patrocinio <patrocinioluisf@gmail.com>
2025-03-27 19:50:31 -0300

    two leds blinking

--- main.c
+++ main.c
@@ -3,15 +3,31 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define PIN_LED 12
+#define PIN_LED_BLUE 12
+#define PIN_LED_RED 13
 
-void vBlinkTask()
+const uint pin_led_blue = PIN_LED_BLUE;
+const uint pin_led_red = PIN_LED_RED;
+
+void setup()
 {
+    stdio_init_all();
+    gpio_init(PIN_LED_BLUE);
+    gpio_set_dir(PIN_LED_BLUE, GPIO_OUT);
+    gpio_init(PIN_LED_RED);
+    gpio_set_dir(PIN_LED_RED, GPIO_OUT);
+}
+
+void vBlinkTask(void *pvParameters)
+{
+    // Type cast the parameter to uint pointer
+    uint *ledPin = (uint *)pvParameters;
+
     while (true)
     {
-        gpio_put(PIN_LED, 1);           // Turn on LED
+        gpio_put(*ledPin, 1);           // Turn on LED
         vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 ms
-        gpio_put(PIN_LED, 0);           // Turn off LED
+        gpio_put(*ledPin, 0);           // Turn off LED
         vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 ms
         printf("Blinking LED\n");
     }
@@ -19,16 +35,14 @@ void vBlinkTask()
 
 int main()
 {
-    stdio_init_all();
-    gpio_init(PIN_LED);
-    gpio_set_dir(PIN_LED, GPIO_OUT);
-    xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+    setup();
+    xTaskCreate(vBlinkTask, "Blue Blink Task", 128, (void *)&pin_led_blue, 1, NULL);
+    xTaskCreate(vBlinkTask, "Red Blink Task", 128, (void *)&pin_led_red, 1, NULL);
     vTaskStartScheduler(); // Start the scheduler
 
-
-    while (true)
+    while (1)
     {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        printf("Main loop\n");
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
     }
 }
